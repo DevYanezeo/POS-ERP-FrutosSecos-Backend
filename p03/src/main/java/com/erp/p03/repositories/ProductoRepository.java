089bdc35320fded922c593b93f6e2992d466caf3
DevYanezeo <eliseo.yanez@usach.cl>
2025-10-13 14:54:44 -0300

    Update ProductoRepository.java
    Se añadio ordenamiento inverso

--- p03/src/main/java/com/erp/p03/repositories/ProductoRepository.java
+++ p03/src/main/java/com/erp/p03/repositories/ProductoRepository.java
@@ -1,24 +1,40 @@
 package com.erp.p03.repositories;
 
+import java.math.BigDecimal;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.erp.p03.entities.ProductoEntity;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<ProductoEntity, Integer>{
+public interface ProductoRepository extends JpaRepository<ProductoEntity, Integer> {
 
+    // Filtros
     List<ProductoEntity> findByEstado(Boolean estado);
     List<ProductoEntity> findByCategoriaId(Integer categoriaId);
     List<ProductoEntity> findByNombreContainingIgnoreCase(String nombre);
     List<ProductoEntity> findByEstadoAndCategoriaId(Boolean estado, Integer categoriaId);
-    List<ProductoEntity> findAllByOrderByNombreAsc();
-    List<ProductoEntity> findAllByOrderByPesoDesc();
-    List<ProductoEntity> findAllByOrderByStockDesc();
+
+    // Filtro por peso y precio
     List<ProductoEntity> findByPesoBetween(int min, int max);
+    List<ProductoEntity> findByPrecioBetween(BigDecimal min, BigDecimal max);
+
+    // Ordenamientos por nombre
+    List<ProductoEntity> findAllByOrderByNombreAsc();
+    List<ProductoEntity> findAllByOrderByNombreDesc();
+
+    // Ordenamientos por peso
+    List<ProductoEntity> findAllByOrderByPesoAsc();
+    List<ProductoEntity> findAllByOrderByPesoDesc();
+
+    // Ordenamientos por stock
+    List<ProductoEntity> findAllByOrderByStockAsc();
+    List<ProductoEntity> findAllByOrderByStockDesc();
+
+    // Ordenamientos por precio
+    List<ProductoEntity> findAllByOrderByPrecioAsc();
+    List<ProductoEntity> findAllByOrderByPrecioDesc();
 
     @Query("SELECT DISTINCT p FROM ProductoEntity p LEFT JOIN FETCH p.lotes l")
     List<ProductoEntity> findAllWithLotes();
